fdb5ab06ba03b8cbd1cb9df5675d7907480be806
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2023-12-22 14:51:20 +0200

    setLevels

--- toolkit/grid.go
+++ toolkit/grid.go
@@ -2,6 +2,7 @@ package toolkit
 
 import (
 	"time"
+
 	"xoney/common/data"
 	"xoney/events"
 	"xoney/exchange"
@@ -23,26 +24,76 @@ func NewGridLevel(price, amount float64, id LevelID) *GridLevel {
 	}
 }
 
+func orderByLevel(level GridLevel, currentPrice float64, symbol data.Symbol) exchange.Order {
+	var side exchange.OrderSide
+
+	if level.price < currentPrice {
+		side = exchange.Buy
+	} else {
+		side = exchange.Sell
+	}
+
+	return *exchange.NewOrder(
+		symbol,
+		exchange.Limit,
+		side,
+		level.price,
+		level.amount,
+	)
+}
+
 type grid struct {
+	symbol   data.Symbol
 	levels   []GridLevel
 	executed LevelID
 	orders   map[LevelID]exchange.Order
 }
 
-func (g *grid) setLevels(levels []GridLevel) {
-	added, modified, canceled := g.checkNewLevels(levels)
-}
-func (g *grid) checkNewLevels(levels []GridLevel) (
-	added    []GridLevel,
-	modified []GridLevel,
-	canceled []GridLevel,
-) {
-	panic("TODO: implement")
-}
-func (g *grid) updateOrders(candle data.Candle) ([]events.Event, error) {
-	panic("TODO: implement")
-}
+func (g *grid) setLevels(levels []GridLevel) []events.Event {
+	orderEvents := make([]events.Event, 0, len(g.levels))
+	
+	// Modified and added levels are processing in g.updateOrders()
+	canceled := g.checkCanceledLevels(levels)
 
+	for _, level := range canceled {
+		order := g.orders[level.id]
+		orderEvents = append(orderEvents, events.NewCloseOrder(order.ID()))
+
+		delete(g.orders, level.id)
+	}
+	return orderEvents
+}
+func (g *grid) checkCanceledLevels(levels []GridLevel) []GridLevel {
+	panic("TODO: implement")
+}
+func (g *grid) updateOrders(candle data.Candle) []events.Event {
+	orderEvents := make([]events.Event, 0, len(g.levels))
+
+	for _, level := range g.levels {
+		order, ok := g.orders[level.id]
+		if ok && order.CrossesPrice(candle.High, candle.Low) {
+			g.executed = level.id
+			delete(g.orders, level.id)
+		}
+
+		if level.id == g.executed {
+			continue
+		}
+
+		orderEvents = append(orderEvents, g.editOrder(level, candle.Close))
+	}
+
+	return orderEvents
+}
+func (g *grid) editOrder(level GridLevel, currPrice float64) events.Event {
+	newOrder := orderByLevel(level, currPrice, g.symbol)
+
+	if order, ok := g.orders[level.id]; ok {
+		return events.NewEditOrder(order.ID(), newOrder)
+	}
+
+	return events.NewOpenOrder(newOrder)
+}
 
 type GridGenerator interface {
 	Instrument() data.Instrument
@@ -71,7 +122,7 @@ func (g *GridBot) Next(candle data.InstrumentCandle) ([]events.Event, error) {
 
 	g.grid.setLevels(levels)
 
-	return g.grid.updateOrders(candle.Candle)
+	return g.grid.updateOrders(candle.Candle), nil
 }
 
 func (g *GridBot) Start(charts data.ChartContainer) error {
